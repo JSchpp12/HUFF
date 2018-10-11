aee7ad021ab3ff16886ef0cf89add0f7d586b416
Jacob Schaupp <jschaup2@rockets.utoledo.edu>
2018-10-11 14:33:36 -0400

    Correctly (maybe?) buildings encoding table !!!!!

--- HUFF/Huffman.cpp
+++ HUFF/Huffman.cpp
@@ -41,7 +41,7 @@ void Huffman::openFile(string fileName)
 	while (inStream >> noskipws >> c)
 	{
 		//cout << c << "\n";
-		
+		numOfCharacters++; 
 		for (int i = 0; i < 256; i++)
 		{
 			if (i == arrayCounter)
@@ -178,7 +178,12 @@ void Huffman::buildTree()
 	rootNode = focus_list[0]; 
 	cout << "COMPLETE !!!!!" << "\n"; 
 	cout << "rootNode weight " << rootNode->weight;
-}
+	cout << "NUM : " << numOfCharacters << "\n"; 
+
+	//_traverse(rootNode);	
+	buildEncodingTable(); 
+	printBinaryTable(); 
+} 
 
 void Huffman::printFOCUS() 
 {
@@ -208,6 +213,11 @@ void Huffman::createParentNode(int index1, int index2)
 	//index 1 will contain the new node, index 2 will be empty hole 
 	//need to fill the hole 
 	focus_list[index1] = &nodeStorage[arrayCounter];
+
+	//set new node as parent to the 2 target nodes 
+	node1->parent = &nodeStorage[arrayCounter]; 
+	node2->parent = &nodeStorage[arrayCounter];
+
 	numOfParentsCreated++; 
 	arrayCounter++; 
 	trimFocus(index2); 
@@ -228,3 +238,158 @@ void Huffman::trimFocus(int empty_index)
 	}
 	focus_listCounter--; 
 }
+
+
+void Huffman::buildEncodingTable()
+{
+	//build binary encoding table from the complete table
+	//traverse the tree for this
+	//right child = 1
+	//left child = 2
+	for (int i = 0; i < arrayCounter; i++)
+	{
+		//binaryRep[i] = traverseForChar(symbolArray[i], "", rootNode);
+		traverseForChar(rootNode, symbolArray[i]); 
+	}
+
+}
+
+void  Huffman::traverseForChar(huff_node* currentNode, char focus)
+{
+	string updated;
+	huff_node* nextNode;
+
+	//going to try and find the thing then return pointer to the node, then follow parents up to root
+	//used to check for nullptr, now I am going to check for symbol leaves should have symbol 
+
+	if (currentNode->symbol == -52)
+	{
+		if (currentNode->leftChild != nullptr)
+		{
+			traverseForChar(currentNode->leftChild, focus);
+		}
+		else
+		{
+			return; 
+		}
+		if (currentNode->symbol != NULL)
+		{
+			if (currentNode->symbol == focus)
+			{
+				findPath(currentNode); 
+			}
+		}
+		if (currentNode->rightChild != nullptr)
+		{
+			traverseForChar(currentNode->rightChild, focus);
+		}
+		else
+		{
+			return; 
+		}
+	}
+	else
+	{
+		//found a node that has a character 
+		if (currentNode->symbol == focus)
+		{
+			findPath(currentNode); 
+		}
+		return; 
+	}
+	
+}
+
+void Huffman::findPath(huff_node* leafNode)
+{
+	huff_node* currentNode, *temp; 
+	currentNode = leafNode; 
+	string constructedPath = ""; 
+
+	//go up to parent of the node I want and check if the node is the right or left child of the parent and append representative bit 
+	while (currentNode != rootNode)
+	{
+		temp = currentNode->parent; 
+		if (temp->rightChild == currentNode)
+		{
+			constructedPath = constructedPath + "1";
+		}
+		else if (temp->leftChild == currentNode)
+		{
+			constructedPath = constructedPath + "0"; 
+		}
+		currentNode = temp; 
+	}
+	foundBinaryPath(constructedPath); 
+}
+	/*
+	void BST::_traverse(Node* in_node)
+{
+	if (in_node != nullptr)
+	{
+		_traverse(in_node->leftChild);
+		cout << in_node->key << " " << in_node->counter << "\n";
+		_traverse(in_node->rightChild);
+	}
+	else
+	{
+		return;
+	}
+}
+
+	if (currentNode != nullptr)
+	{
+		if (currentNode->symbol != NULL)
+		{
+			if (currentNode->symbol == focus)
+			{
+				updated = binaryPath + "0";
+				nextNode = currentNode->leftChild;
+				traverseForChar(focus, updated, nextNode);
+				updated = binaryPath + "1";
+				nextNode = currentNode->rightChild;
+			}
+		}
+		else
+		{
+			foundBinaryPath(binaryPath); 
+		}
+	}
+	else 
+	{
+		return; 
+	}
+}
+*/ 
+
+void Huffman::foundBinaryPath(string binaryPath)
+{
+	binaryRepList[binaryArrayCounter] = binaryPath; 
+	binaryArrayCounter++; 
+}
+
+void Huffman::printBinaryTable()
+{
+	cout << "Printing binary table \n"; 
+	for (int i = 0; i < arrayCounter; i++)
+	{
+		cout << symbolArray[i] << " : " << binaryRepList[i] << "\n"; 
+	}
+}
+
+void Huffman::_traverse(huff_node* in_node)
+{
+	if (in_node->symbol == -52)
+	{
+		_traverse(in_node->leftChild);
+		cout << in_node->symbol << " " << in_node->weight << "\n";
+		_traverse(in_node->rightChild);
+	}
+	else
+	{
+		cout << in_node->symbol << " " << in_node->weight << "\n";
+		return;
+	}
+}
+
+
--- HUFF/Huffman.h
+++ HUFF/Huffman.h
@@ -13,6 +13,7 @@ public:
 		huff_node* parent; 
 		huff_node* rightChild; 
 		huff_node* leftChild; 
+		string contained_symbols; 
 		int weight; 
 		char symbol; 
 	};
@@ -20,10 +21,15 @@ public:
 	huff_node* rootNode; 
 	huff_node nodeStorage[2000];
 
+	//use this to store the binary representation of each character in the encoding table 
+	string binaryRepList[250]; 
+
 	//use this array to build the tree and stuff
 	huff_node* focus_list[2000]; 
 	int focus_listCounter = 0; 
-	int arrayCounter = 0; 
+	int arrayCounter = 0;
+	int numOfCharacters = 0; 
+	int binaryArrayCounter = 0; 
 	int numOfParentsCreated = 0; 
 	int weightArray[256];
 	char symbolArray[256];
@@ -35,6 +41,8 @@ public:
 	void DecodeFile(string inputFile, string OutputFile); 
 	void EncodeFileWithTree(string inputFile, string OutputFile); 
 	void DisplayHelp();
+	void foundBinaryPath(string binaryPath); 
+	
 
 private: 
 	void openFile(string fileName); 
@@ -45,5 +53,10 @@ private:
 	void createParentNode(int index1, int index2); 
 	huff_node* findNextLowest(int lowestVal); 
 	void trimFocus(int empty_index); 
+	void buildEncodingTable();
+	void traverseForChar(huff_node* currentNode, char focus);
+	void printBinaryTable(); 
+	void findPath(huff_node* leaf_node); 
+	void _traverse(huff_node * in_node); 
 };
 
